fbe317a6f1cd1b5371ed139386d2f10bea85893a
sairam-maurya <rams44258@gmail.com>
2023-06-24 07:56:40 -1200

    template string

---
+++ templateString.js
@@ -0,0 +1,12 @@
+const recipient = "sai ram"
+const sender ="sam "
+
+// const email = "hey " + recipient + "! how is it going"
+
+// console.log(email)
+
+//teplate string
+
+const email = ` hey ${recipient} ! how is it going ? cheers ${sender}`
+
+console.log(email)
