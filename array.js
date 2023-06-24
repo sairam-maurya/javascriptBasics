ce70c386436a1daeb77d2b83ff49eb31534f4902
sairam-maurya <rams44258@gmail.com>
2023-06-24 01:26:57 -1200

    array

---
+++ array.js
@@ -0,0 +1,18 @@
+let largestCountries = ["Tuvalu","India","Indonesia","Monaco"]
+
+
+//china pakistan
+
+largestCountries.push("pakistan")
+
+console.log(largestCountries)
+
+largestCountries.unshift("china")
+console.log(largestCountries)
+
+largestCountries.pop()
+largestCountries.shift()
+console.log(largestCountries)
+
+
+
